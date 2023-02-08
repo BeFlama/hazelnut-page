152070d9ea611000b2283090bf0b88168a4410b7
BeFlama <florencia.n.benitez@hotmail.com>
2023-02-08 01:01:07 -0300

    listo btn search

--- JS/pagprods.js
+++ JS/pagprods.js
@@ -1,81 +1,144 @@
-/* 1 */
-/* function abrirDeposito(array){
-} */
-/* DOM */
+/* elemtnos del html */
 let divdeposito = document.getElementById("deposito")
+let inputSearch = document.getElementById("inputSearch")
+let btnOrder = document.getElementById("btnOrder")
+let btnCarr = document.getElementById("btnCarr")
+let precioTotal = document.getElementById("precioTotal")
 
-for(let mueble of deposito){
-    let divMueble = document.createElement("div")
+/* let bodyCarrito = document.getElementsById("bodyCarrito") */
+
+
+abrirDeposito(deposito)
+
+/* STORAGE CARRITO */
+let carritoCompra
+
+if(localStorage.getItem("carrito")){
+    carritoCompra = JSON.parse(localStorage.getItem("carrito"))
+}else{
+    carritoCompra = []
+    localStorage.setItem("carrito", carritoCompra)
+}
+
+
+/* DOM */
+function abrirDeposito(array){
+    for(let mueble of array){
+        let divMueble = document.createElement("div")
+        
+        divMueble.innerHTML = `
     
-    divMueble.innerHTML = `
-
-    <div class="card_prod-img">
-        <img src="../assets/img/imgprod/${mueble.imagen}" alt="${mueble.tipo}/${mueble.nombre}">
-    </div>
-    <div class="card_prod-text">
-        <h5>${mueble.tipo} - ${mueble.nombre}</h5>
-        <p><a href="#">Detalles del producto</a></p>
-        <strong>$${mueble.precio}</strong>
-        <button>Comprar</button>
-    </div>
-
-    `
-    divdeposito.appendChild(divMueble)
-}
-
-
-/* 2 */
-function buscarMueble(array){
-    let muebleBuscado = prompt("Ingresa el tipo de mueble que buscas")
-    let busquedaTipo = array.filter(
-        (mueble)=> mueble.tipo.toLowerCase() == muebleBuscado.toLowerCase()
-    )
-    if(busquedaTipo.length == 0){
-        console.log(`Disculpa, "${muebleBuscado}" no se encuentra en nuestro catálogo`)
-    }else{
-        console.log(`Para tu busqueda de "${muebleBuscado}"`)
-        abrirDeposito(busquedaTipo)
+        <div class="card_prod-img" id=${mueble.id}>
+            <img src="../assets/img/imgprod/${mueble.imagen}" alt="${mueble.tipo}/${mueble.nombre}">
+        </div>
+        <div class="card_prod-text">
+            <h5>${mueble.tipo} - ${mueble.nombre}</h5>
+            <p><a href="#">Detalles del producto</a></p>
+            <strong>$${mueble.precio}</strong>
+            <button id="agregarBtn${mueble.id}">Comprar</button>
+        </div>
+    
+        `
+        divdeposito.appendChild(divMueble)
+        let agregarBtn = document.getElementById(`agregarBtn${mueble.id}`)
+        console.log(agregarBtn)
+        agregarBtn.onclick = ()=>{
+            agregarAlCarrito(mueble)
+        }
     }
 }
 
-/* 3 */
-function comprarMueble(){
-/*     console.log(`A partir del cátalogo ingrese el id del mueble que desea comprar:`)
-    for(let elem of array){
-        console.log(`${elem.id} - ${elem.tipo} - ${elem.nombre} - ${elem.precio}`)
-    }
-    let idElegido = parseInt(prompt("ingrese el id "))
+/* carrito de compras */
 
-    const newArray = array.slice(idElegido, (idElegido + 1))
-
-    for(let elem of newArray){
-        console.log(`${elem.id} - ${elem.tipo} - ${elem.nombre} - ${elem.precio}`)
-    } */
-    let opcionBuy = parseInt(prompt(`
-    1 - Ver stock
-    2 - 3 cuotas sin interes
-    3 - Quiero calculcar cuotas (+25% de interes)
-    4 - Voy a pagar en efectivo o transferencia (10% de descuento)`))
-    switch(opcionBuy){
-        case 1:
-            abrirDeposito(deposito)
-        break
-        case 2:
-            cuotasTres()
-        break
-        case 3:
-            calculcarCuotas()
-        break
-        case 4:
-            calculcarDesc()
-        break
-        default:
-            console.log(`${opcionBuy} no es valido`)
-        break
-    }
+function agregarAlCarrito(mueble){
+    console.log(`el producto ${mueble.tipo} - ${mueble.nombre} se agregó`)
+    carritoCompra.push(mueble)
+    localStorage.setItem("carrito", JSON.stringify(carritoCompra))
+    console.log(carritoCompra)
 } 
 
-function cuotasTres(){
+function cargarProducto(array){
+    console.log("funciona el carrito")
+    bodyCarrito.innerHTML = ""
+    array.forEach((prodCarrito)=>{
+        console.log(prodCarrito.nombre)
+        bodyCarrito.innerHTML += `
+        <div class="card mb-3" style="max-width: 540px;" id="cardProd${prodCarrito.id}">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="../assets/img/imgprod/${prodCarrito.imagen}" class="img-fluid rounded-start" height = "20px" alt="${prodCarrito.tipo} ${prodCarrito.nombre}">
+                </div>
+            <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title">${prodCarrito.tipo} ${prodCarrito.nombre}</h5>
+            <strong>$${prodCarrito.precio}</strong>
+                <div class="d-grid gap-2 d-md-block">
+                <button class="btn btn-primary" type="button">Modificar</button>
+                <button class="btn btn-primary" id= "supr${prodCarrito.id}" type="button">Eliminar</button>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    `
+    })
+    array.forEach((prodCarrito)=>{
+        document.getElementById(`supr${prodCarrito.id}`).addEventListener("click", ()=>{
+            let cardProd = document.getElementById(`cardProd${prodCarrito.id}`)
+            cardProd.remove()
+
+            let prodEliminar = array.find(mueble => mueble.id == prodCarrito.id)
+            console.log(prodEliminar)
+
+            let posicion = array.indexOf(prodEliminar)
+            console.log(posicion)
+
+            array.splice(posicion, 1)
+            console.log(array)
+
+            localStorage.setItem("carrito", JSON.stringify(array))
+
+            compraTotal(array)
+        })
+    })
+    compraTotal(array)
+}
+
+function compraTotal(array){
+    let total = array.reduce((acc, prodCarrito)=> acc + prodCarrito.precio , 0)
+    console.log("precio total" + total)
+    total == 0 ?
+    precioTotal.innerHTML = `Todavia no agregaste ningun producto` :
+    precioTotal.innerHTML = `El total de tu compra es <strong>$${total}</strong>`
+    return total
+}
+
+/* busqueda */
+
+function buscarProd(buscado, array){
+    let busquedaArray = array.filter(
+        (mueble) => mueble.tipo.toLocaleLowerCase().includes(buscado.toLowerCase()) || mueble.nombre.toLocaleLowerCase().includes(buscado.toLowerCase())
+    )
+
+    busquedaArray.length == 0 ? 
+    (divdeposito.innerHTML = `<h3 class="prod404" >No encontramos lo que buscas</h3>`, 
+    abrirDeposito(busquedaArray))
+    : (divdeposito.innerHTML = "", abrirDeposito(busquedaArray))
+}
+
+//EVENTOS
+btnCarr.addEventListener("click", ()=>{
+    cargarProducto(carritoCompra)
+})
+
+inputSearch.addEventListener("input", ()=>{
+    buscarProd(inputSearch.value.toLowerCase(), deposito)
+})
+
+
+
+
+/* function cuotasTres(){
     let precio = parseInt(prompt("Ingresa el precio del mueble que queres comprar"))
     let precioCuotas = (precio / 3)
 
@@ -97,10 +160,10 @@ function calcularDesc(){
     let precioFinalDesc = (precioDesc - (precioDesc * (10 / 100)))
 
     alert(`el precio del mueble que elegiste es ${precioDesc} pesos, en efectivo o transferencia quedaria en ${precioFinalDesc} pesos`)
-}
+} */
 
-/* 4 */
-function ordenar(array){
+
+/* function ordenar(array){
     let opcion = parseInt(prompt(`
     1 - Ordenar por precio, mayor a menor
     2 - Ordenar por precio, menor a mayor`))
@@ -127,10 +190,10 @@ function ordenarMenor(array){
     const menorMayor = [].concat(array)
     menorMayor.sort((param1, param2)=> param1.precio - param2.precio)
     abrirDeposito(menorMayor)
-}
+} */
 
 /* 5 */
-function reciclarMueble(array){
+/* function reciclarMueble(array){
     let muebleIngresado = prompt(`Que tipo de mueble tenes?
     Recorda que solo aceptamos del tipo que vendemos`)
 
@@ -152,53 +215,8 @@ function reciclarMueble(array){
     }else{
         console.log("Disculpa, no aceptamos este tipo de mueble")
     }
-}
+} */
 
-/* MENU INICIAL */
-
-/* let salirMenu = true
-
-do{
-    let opcionMenu = prompt(`Ingrese su opcion requerida
-    1 - Ver stock de muebles
-    2 - Buscar muebles por categoría
-    3 - Comprar mueble
-    4 - Ordenar catalogo por precio
-    5 - Quiero vender mi propio mueble
-    6 - Salir del menú`)
-
-    switch(opcionMenu){
-        case "1":
-            abrirDeposito(deposito)
-        break
-
-        case "2":
-            buscarMueble(deposito)
-        break
-
-        case "3":
-            comprarMueble(deposito)
-        break
-
-        case "4":
-            ordenar(deposito)
-        break
-
-        case "5":
-            reciclarMueble(deposito)
-        break
-
-        case "6":
-            alert("El menú se cerró")
-            salirMenu=false
-        break
-
-        default:
-            alert("Opción NO válida")
-        break
-}
-}while(salirMenu)
- */
 
 
 
